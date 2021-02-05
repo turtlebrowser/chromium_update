f8c91d5923450934865c8b0565b34bbaddc76702
Patricia Aas <patricia@turtlesec.no>
2021-02-05 11:48:32 +0100

    Clean up the bat files a bit

--- build_qt.bat
+++ build_qt.bat
@@ -1,5 +1,6 @@
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
-SET _ROOT=C:\Code\qt-everywhere-src-5.15.2
-SET PATH=%_ROOT%\qtbase\bin;%_ROOT%\gnuwin32\bin;%PATH%
-SET _ROOT=
-CALL jom -j 12
+SET QT_DIRECTORY=C:\Code\qt-everywhere-src-5.15.2
+SET CHROMIUM_WINDOWS_SDK_VERSION=10.0.19041.0
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %CHROMIUM_WINDOWS_SDK_VERSION%
+SET PATH=%QT_DIRECTORY%\qtbase\bin;%QT_DIRECTORY%\gnuwin32\bin;%QT_DIRECTORY%\qtwebengine\src\3rdparty\chromium\third_party\llvm-build\Release+Asserts\bin;%PATH%
+SET DEPOT_TOOLS_WIN_TOOLCHAIN=0 
+CALL jom -k -j 12
--- configure_qt.bat
+++ configure_qt.bat
@@ -1,5 +1,7 @@
-CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 10.0.19041.0
-SET _ROOT=C:\Code\qt-everywhere-src-5.15.2
-SET PATH=%_ROOT%\qtbase\bin;%_ROOT%\gnuwin32\bin;%PATH%
-SET _ROOT=
-CALL ..\qt-everywhere-src-5.15.2\configure.bat -developer-build -opensource -confirm-license -nomake examples -nomake tests -platform win32-clang-msvc
+SET QT_DIRECTORY=C:\Code\qt-everywhere-src-5.15.2
+SET NINJAFLAGS=-k 0
+SET COMMON_CONFIGURE_FLAGS=-developer-build -opensource -confirm-license -nomake examples -nomake tests
+SET CHROMIUM_WINDOWS_SDK_VERSION=10.0.19041.0
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64 %CHROMIUM_WINDOWS_SDK_VERSION%
+SET PATH=%QT_DIRECTORY%\qtbase\bin;%QT_DIRECTORY%\gnuwin32\bin;%QT_DIRECTORY%\qtwebengine\src\3rdparty\chromium\third_party\llvm-build\Release+Asserts\bin;%PATH%
+CALL ..\qt-everywhere-src-5.15.2\configure.bat -%COMMON_CONFIGURE_FLAGS% -platform win32-clang-msvc
