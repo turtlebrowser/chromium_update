3b6882488759df94029e91d288a9f6e34956f850
Patricia Aas <patricia@turtlesec.no>
2021-03-11 21:50:26 +0100

    First draft of script to build llvm on windows

---
+++ build_llvm.bat
@@ -0,0 +1,4 @@
+CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+set LLVM_ROOT=C:/Code/qt-everywhere-src-5.15.2/qtwebengine/src/3rdparty/chromium/third_party/llvm-build/Release+Asserts/bin
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DLLVM_OPTIMIZED_TABLEGEN=On -DLLVM_ENABLE_PDB=ON -DLLVM_ENABLE_LLD=True -DLLVM_ENABLE_PROJECTS='chrometools;llvm;clang;clang-tools-extra;lld;lldb;compiler-rt;libcxx;libcxxabi;libunwind' -DLIBCXXABI_USE_LLVM_UNWINDER=ON -DLIBCXX_CXX_ABI=default -DCMAKE_C_COMPILER="%LLVM_ROOT%/clang-cl.exe" -DCMAKE_CXX_COMPILER="%LLVM_ROOT%/clang-cl.exe" -DCMAKE_LINKER="%LLVM_ROOT%/lld-link.exe" -DLLVM_USE_CRT_RELEASE=MT ..\llvm
+cmake --build .
